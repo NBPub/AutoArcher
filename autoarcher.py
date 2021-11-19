b84cf0f388728f431294f82b4b6528881a146c40
NBPub <80983660+NBPub@users.noreply.github.com>
2021-11-19 11:56:56 -0800

    Create autoarcher.py
    Main function

---
+++ autoarcher.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 15:01:36 2021
+
+@author: NBPub
+"""
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+from dotenv import load_dotenv
+from base import Startup, Login, Navigator, unblockIP
+from time import sleep
+
+load_dotenv()
+delay = int(os.environ.get("page_wait"))
+
+driver = Startup()
+print('Opening login page')
+sleep(delay/2)
+
+while driver.current_url.find('login') > 0:
+    Login(driver)
+    print('Logging in')
+    sleep(1)
+
+if driver.current_url.find('login') < 0:
+    print('Navigating to page')
+    sleep(delay)
+    Navigator(driver)
+    sleep(delay)
+    
+    print('Checking if phone is blocked')
+    result = unblockIP(driver)
+    print(result)
+    
+driver.quit()
